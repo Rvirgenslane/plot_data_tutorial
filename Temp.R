775857c8429617fbf5d96148a3a17f663a7a766a
Richard <rvs.biotools@gmail.com>
2023-12-16 18:28:25 -0800

    Update Temp.R

--- Temp.R
+++ Temp.R
@@ -3,26 +3,26 @@ library(shinyjs)
 
 ui <- fluidPage(
   useShinyjs(),
-  HTMLOutput("seasonal_animation"),
-  sliderInput("season_selector", "Select Season", 1, 4, 1)
+  HTMLOutput("holiday_animation"),
+  selectInput("holiday_selector", "Select Holiday", c("New Year's Day", "Independence Day", "Thanksgiving"))
 )
 
 server <- function(input, output) {
-  output$seasonal_animation <- renderUI({
-    season <- input$season_selector
+  output$holiday_animation <- renderUI({
+    holiday <- input$holiday_selector
     
-    # HTML and CSS code for different seasons
+    # HTML and CSS code for different holidays
     html_code <- sprintf('
-      <div id="seasonal_div" class="season%s">
-        <p>Season %s</p>
+      <div id="holiday_div" class="animated">
+        <p>%s</p>
       </div>
-    ', season, season)
+    ', holiday)
     
-    # JavaScript to handle animation logic (CSS transitions)
+    # JavaScript to handle animation logic (animate.css)
     js_code <- '
-      shinyjs.toggleSeason = function(season) {
-        $("#seasonal_div").removeClass().addClass("season" + season);
-      }
+      $(document).ready(function(){
+        $("#holiday_div").addClass("animate__animated animate__fadeIn");
+      });
     '
     
     # Using shinyjs to run the JavaScript code
